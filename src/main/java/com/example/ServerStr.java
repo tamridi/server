5d3eb39be1915ccaf75bd507a61a8bf3dc9c4485
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 09:20:11 +0200

    commit1

---
+++ src/main/java/com/example/ServerStr.java
@@ -0,0 +1,17 @@
+package com.example;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerSt {
+    ServerSocket server =null;
+    Socket client =null;
+    String stringaRicevuta =null;
+    String stringaModificata =null;
+    BufferedReader inDalClient;
+    DataOutputStream outVersoClient;
+
+    public Socket attendi(){
+        
+    }
+}
